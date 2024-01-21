e3eb695c8906654d01693b43b7a5e858d5048529
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-01-21 14:03:24 +0300

    create navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import Link from "next/link";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 p-5 border-b mb-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
