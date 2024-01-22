a456e163cdf319f5569f2f550b67b454e0647269
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-01-22 19:16:38 +0300

    added a spinner component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,5 @@
 "use client";
-import {
-  Button,
-  Callout,
-  Text,
-  TextField,
-  TextFieldInput,
-} from "@radix-ui/themes";
+import { Button, Callout, TextField, TextFieldInput } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import axios from "axios";
@@ -16,11 +10,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
   const {
     register,
     control,
@@ -41,9 +37,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An Unexpected Error Occured");
           }
         })}
@@ -60,7 +58,9 @@ const NewIssuePage = () => {
           )}
         />
         {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
