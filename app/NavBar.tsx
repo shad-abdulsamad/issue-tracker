9ac74ac4f26148c3784adae99d3c8b6c2b00723e
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-01-27 17:11:14 +0300

    added a loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
+import { Skeleton } from "@/app/components";
 import {
   Avatar,
   Box,
@@ -45,8 +46,9 @@ const NavLinks = () => {
         <li>
           <Link
             className={classNames({
-              "nav-link": true,
-              "!text-zinc-950": link.href === currentPath,
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover: text-zinc-800 transition-colors": true,
             })}
             href={link.href}
           >
@@ -61,7 +63,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
   if (status === "unauthenticated")
     return (
       <Link className="nav-link" href={"/api/auth/signin"}>
