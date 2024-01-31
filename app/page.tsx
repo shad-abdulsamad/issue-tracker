061cf44dc00f281f04f3a167aaddf558fb2592dc
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-01-31 20:10:59 +0300

    built the pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+interface Props {
+  searchParams: { page: string };
+}
+
+export default function Home({ searchParams }: Props) {
   return (
     <div>
-      Hello World
-      <Pagination currentPage={2} itemCount={100} pageSize={10} />
+      <Pagination
+        currentPage={parseInt(searchParams.page)}
+        itemCount={100}
+        pageSize={10}
+      />
     </div>
   );
 }
